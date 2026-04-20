6371cdb0376757f46d9e678bad68c8920706ca6e
Roberto Monzón <rm15830921@outlook.com>
2026-04-19 22:20:03 -0600

    creation of a communication file with the database

---
+++ R-Manager/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace R_Manager
+{
+    internal class Database
+    {
+        private string dbPath;
+
+        public Database()
+        {
+            string folder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AppData");
+            Directory.CreateDirectory(folder);
+
+            dbPath = Path.Combine(folder, "R-Manager.db");
+        }
+
+        public SqliteConnection GetConnection()
+        {
+            return new SqliteConnection($"Data Source={dbPath}");
+        }
+    }
+}
